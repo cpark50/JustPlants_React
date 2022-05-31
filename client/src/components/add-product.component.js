08a55c2345f8f5694c1f8f5231ccd7cf1b062622
Cindy Park <ccindyy82021@gmail.com>
2022-05-30 17:25:47 -0700

    add component updated

--- client/src/components/add-product.component.js
+++ client/src/components/add-product.component.js
@@ -4,11 +4,13 @@ import ProductDataService from "../services/product.service";
 export default class AddProduct extends Component {
   constructor(props) {
     super(props);
+    this.onChangeId = this.onChangeId.bind(this);
     this.onChangeName = this.onChangeName.bind(this);
     this.onChangePrice = this.onChangePrice.bind(this);
     this.onChangeDescription = this.onChangeDescription.bind(this);
     this.onChangeWater = this.onChangeWater.bind(this);
     this.onChangeLight = this.onChangeLight.bind(this);
+    this.uploadPicture = this.uploadPicture.bind(this);
     this.onChangeFriendly = this.onChangeFriendly.bind(this);
     this.saveProduct = this.saveProduct.bind(this);
     this.newProduct = this.newProduct.bind(this);
@@ -23,6 +25,12 @@ export default class AddProduct extends Component {
       pet: false,
       submitted: false
     };
+  }
+  
+  onChangeId(e) {
+    this.setState({
+      id: e.target.value
+    });
   }
 
   onChangeName(e) {
@@ -61,19 +69,34 @@ export default class AddProduct extends Component {
     });
   }
 
+  uploadPicture(e) {
+    this.setState({
+      picturePreview : URL.createObjectURL(e.target.files[0]),
+      pictureAsFile : e.target.files[0]
+    });
+  }
+
   saveProduct() {
     var data = {
-      title: this.state.title,
-      description: this.state.description
+      id: this.state.id,
+      name: this.state.name,
+      price: this.state.price,
+      description: this.state.description,
+      water: this.state.water,
+      light: this.state.light,
+      pet: this.state.pet
     };
 
     ProductDataService.create(data)
       .then(response => {
         this.setState({
           id: response.data.id,
-          title: response.data.title,
+          name: response.data.name,
+          price: response.data.price,
           description: response.data.description,
-          published: response.data.published,
+          water: response.data.water,
+          light: response.data.light,
+          pet: response.data.pet,
 
           submitted: true
         });
@@ -110,6 +133,18 @@ export default class AddProduct extends Component {
           </div>
         ) : (
           <div>
+            <div className="form-group">
+              <label htmlFor="id">id</label>
+              <input
+                type="number"
+                className="form-control"
+                id="id"
+                required
+                value={this.state.id}
+                onChange={this.onChangeId}
+                name="id"
+              />
+            </div>
             <div className="form-group">
               <label htmlFor="name">Plant Name</label>
               <input
@@ -190,7 +225,15 @@ export default class AddProduct extends Component {
                 name="pet"
               />
             </div>
-
+            <div className="form-group">
+              <label htmlFor="image">Product Image</label>
+              <input
+                type="file"
+                className="form-control"
+                onChange={this.uploadPicture}
+                name="image"
+              />
+            </div>
             <button onClick={this.saveProduct} className="btn btn-success">
               Submit
             </button>
